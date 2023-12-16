efb7fbc7052c7302e619a108b12c02275d3e5890
BardMleczny <dominopl2016@wp.pl>
2023-12-16 22:39:03 +0100

    Update Player.cpp

--- Projekt INT/src/Player.cpp
+++ Projekt INT/src/Player.cpp
@@ -20,16 +20,16 @@ void Player::Move()
 	m_speedY = 0;
 
 	if (Input::isKeyDown(GLFW_KEY_A)){
-		m_speedX = -3.0f;
+		m_speedX += -3.0f;
 	}
 	if (Input::isKeyDown(GLFW_KEY_D)) {
-		m_speedX = 3.0f;
+		m_speedX += 3.0f;
 	}
 	if (Input::isKeyDown(GLFW_KEY_S)) {
-		m_speedY = -3.0f;
+		m_speedY += -3.0f;
 	}
 	if (Input::isKeyDown(GLFW_KEY_W)) {
-		m_speedY = 3.0f;
+		m_speedY += 3.0f;
 	}
 	m_rectangle.m_x += m_speedX;
 	m_rectangle.m_y += m_speedY;

feca6890754ab613c5647639bc8f16276c98d795
Traikocik <wanilia12@interia.pl>
2023-12-23 01:41:14 +0100

    Small terrain changes

--- Projekt INT/include/TerrainType.h
+++ Projekt INT/include/TerrainType.h
@@ -2,5 +2,12 @@
 
 enum TerrainType
 {
-
+	dirtLeftTop = 140065000,
+	dirtRightTop = 145070000,
+	dirtCenter = 150075000,
+	dirtLeftBottom = 155080000,
+	dirtRightBottom = 160085000,
+	grassLeft = 000250000/*,
+	grassCenter = 000255000, s¹ zera na pocz¹tku, no i problem, mo¿e jakiœ obiekt kolor zamiast tego lub struct
+	grassRight = 005255005*/
 };
--- Projekt INT/src/Terrain.cpp
+++ Projekt INT/src/Terrain.cpp
@@ -8,11 +8,21 @@ struct Tile
 
 Tile getTileType(TerrainType type)
 {
-	/*
-		switch z texturami
-	*/
-
-	return { { 1.0f, 1.0f, 1.0f, 1.0f }, Texture("res/textures/image1.png") };
+	switch (type)
+	{
+	case dirtLeftTop:
+		return { { 0.0f, 0.0f, 0.0f, 1.0f }, Texture("res/textures/dirtLeftTop.png") };
+	case dirtRightTop:
+		return { { 0.0f, 0.0f, 0.0f, 1.0f }, Texture("res/textures/dirtRightTop.png") };
+	case dirtCenter:
+		return { { 0.0f, 0.0f, 0.0f, 1.0f }, Texture("res/textures/dirtCenter.png") };
+	case dirtLeftBottom:
+		return { { 0.0f, 0.0f, 0.0f, 1.0f }, Texture("res/textures/dirtLeftBottom.png") };
+	case dirtRightBottom:
+		return { { 0.0f, 0.0f, 0.0f, 1.0f }, Texture("res/textures/dirtRightBottom.png") };
+	default:
+		return { { 0.0f, 0.0f, 0.0f, 1.0f }, Texture("res/textures/image2.png") };
+	}
 }
 
 Terrain::Terrain(Rectangle& rectangle, TerrainType type)
